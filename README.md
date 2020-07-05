ea46e4828eda98fbe13b0642b97e50013f88fc10
Daniella Skinner <daniellaskinner@macbook-pro.home>
2020-07-05 13:52:12 +0100

    added movement function and updated readme for game aim

--- README.md
+++ README.md
@@ -11,5 +11,11 @@
 ## To run on your machine run: npm install.
 
 ## How to play the game:
-### Click on froggy's lilypad to reveal a word to practise, once you have said the word click on froggy. Who know's how many time you have to nudge him
-until he wakes up!
+### Move froggy to the lilypad to reveal a word to practise, once you have said the word move to find the next lilypad.
+
+##notes for nelly:
+- ribbet noise when land on lilypad
+- when land on lilypad word pops up on screen- modal or something, the game freezes for 30 seconds
+- later on could add a timer option for the therapist/adult to select how long (s) they want the word to remain on the screen
+- word/modal dissapears and another lilypad pops up somewhere else
+- in future to add record the child's iteration of the word and compare to mark it
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,47 @@
 import * as Phaser from 'phaser';
 
+//config stuff for the scene
+const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
+    active: false,
+    visible: false,
+    key: 'Game',
+};
+
+//set up a scene and pass in the scene config object
+export class GameScene extends Phaser.Scene {
+    private square: Phaser.GameObjects.Rectangle & { body: Phaser.Physics.Arcade.Body };
+
+    constructor() {
+        super(sceneConfig);
+    }
+
+    public create() {
+        this.square = this.add.rectangle(400, 400, 100, 100, 0x8FBC8F) as any;
+        this.physics.add.existing(this.square);
+    }
+
+    public update() {
+        const cursorKeys = this.input.keyboard.createCursorKeys();
+
+        if (cursorKeys.up.isDown) {
+            this.square.body.setVelocityY(-500);
+        } else if (cursorKeys.down.isDown) {
+            this.square.body.setVelocityY(500);
+        } else {
+            this.square.body.setVelocityY(0);
+        }
+
+        if (cursorKeys.right.isDown) {
+            this.square.body.setVelocityX(500);
+        } else if (cursorKeys.left.isDown) {
+            this.square.body.setVelocityX(-500);
+        } else {
+            this.square.body.setVelocityX(0);
+        }
+    }
+}
+
+//setting up our game config object
 const gameConfig: Phaser.Types.Core.GameConfig = {
     title: 'Sample',
 
@@ -19,6 +61,8 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
 
     parent: 'game',
     backgroundColor: '#000000',
+    scene: GameScene,
 };
 
+//instantiate a new phaser game object, passing in the game config object
 export const game = new Phaser.Game(gameConfig);
