1a29917b782f668d936159f2b12d8a924f5f38a6
nelly.skinner <daniellaskinner120@gmail.com>
2020-07-19 13:01:03 +0100

    changed frog to phaser arcade sprite rather than phaser game object, re-factored update method

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -9,7 +9,7 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
 
 //set up a scene and pass in the scene config object
 export class GameScene extends Phaser.Scene {
-    private froggy : Phaser.GameObjects.Sprite;
+    private froggy: Phaser.Physics.Arcade.Sprite;
 
     constructor() {
         super(sceneConfig);
@@ -23,8 +23,8 @@ export class GameScene extends Phaser.Scene {
         this.froggy = this.physics.add.sprite(200, 200, 'froggy');
         this.froggy.setScale(0.3, 0.3);
         // this.froggy.displayWidth = this.config.width * .1; this.froggy.scaleY = this.froggy.scaleX.
-        // froggy.setBounce(0.2);
-        // this.froggy.
+        // this.froggy.setBounce(0.2);
+
         this.physics.add.existing(this.froggy);
 
         // player.setCollideWorldBounds(true);
@@ -33,22 +33,40 @@ export class GameScene extends Phaser.Scene {
     public update() {
         const cursorKeys = this.input.keyboard.createCursorKeys();
 
-        if (cursorKeys.up.isDown) {
-            this.froggy.y -= 10;
-        } else if (cursorKeys.down.isDown) {
-            this.froggy.y += 10;
-        } else {
-            this.froggy.y += 0;
+        if (cursorKeys.left.isDown)
+        {
+            this.froggy.setVelocityX(-150);
+
+            // this.froggy.anims.play('left', true);
+        }
+        else if (cursorKeys.right.isDown)
+        {
+            this.froggy.setVelocityX(150);
+
+            // this.froggy.anims.play('right', true);
+        }
+        else if (cursorKeys.down.isDown)
+        {
+            this.froggy.setVelocityY(150);
+
+            // this.froggy.anims.play('down', true);
+        }
+        else if (cursorKeys.up.isDown)
+        {
+            this.froggy.setVelocityY(-150);
+
+            // this.froggy.anims.play('up', true);
+        }
+        else
+        {
+            this.froggy.setVelocity(0, 0);
+
+            // this.froggy.anims.play('turn');
         }
 
-        if (cursorKeys.right.isDown) {
-            this.froggy.x += 10;
-        } else if (cursorKeys.left.isDown) {
-            this.froggy.x -= 10;
-        } else {
-            this.froggy.x += 0;
+        if (cursorKeys.space.isDown) {
+            this.froggy.setBounceY(0.2);
         }
-
         // if (cursorKeys.up.isDown && this.froggy.body.touching.down)
         // {
         //     this.froggy.setVelocityY(-330);
